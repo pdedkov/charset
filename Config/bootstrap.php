97d47cb1e47b73cabfc73e2c464a50a41d267876
Pavel E. Dedkov <pavel.dedkov@gmail.com>
2017-10-30 15:56:35 +0300

    psr-4

--- Config/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Частотные таблицы символов для разных кодировок
-Configure::write('Charset.encodings', [
-	'windows-1251'	=> require APP . 'Vendor' . DS . 'char_spec_w1251.php',
-	'koi8-r'				=> require APP . 'Vendor' . DS . 'char_spec_koi8.php',
-	'iso-8859-1'		=> require APP . 'Vendor' . DS . 'char_spec_iso8859.php'
-]);
